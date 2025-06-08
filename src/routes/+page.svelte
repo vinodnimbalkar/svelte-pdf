49b58f9a9ccfd848e0c540d1dd72cd284ef539e4
vinodnimbalkar <omvinod@yahoo.com>
2025-06-08 20:17:46 +0530

    revert: ⏪ Remove demo examples from page route
    Reverted demo examples of externalLinksTarget usage from the main page
    route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,4 @@
     import PdfViewer from '$lib/PdfViewer.svelte';
 </script>
 
-<!-- Default behavior (opens in new tab) -->
 <PdfViewer url='./sample.pdf' />
-
-<!-- Explicitly set to open in new tab -->
-<PdfViewer url='./sample.pdf' externalLinksTarget="_blank" />
-
-<!-- Open in same tab -->
-<PdfViewer url='./sample.pdf' externalLinksTarget="_self" />
-
-<!-- Open in parent frame -->
-<PdfViewer url='./sample.pdf' externalLinksTarget="_parent" />
